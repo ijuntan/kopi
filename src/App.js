a2edc55413beac0010a2e16243e5c60854a6701d
Junior Tanaya <juniortanaya@gmail.com>
2020-07-11 17:56:17 +0700

    second build

--- src/App.js
+++ src/App.js
@@ -1,79 +1,132 @@
 import React,{useRef} from 'react';
-import {AppBar,Toolbar,CssBaseline,Grid,Button} from '@material-ui/core';
-import Produk from './Product.js';
+import {AppBar,Toolbar,CssBaseline,Grid,Button,Paper} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import FBwhite from './Images/fw.png';
+import FBbrown from './Images/fb.png';
+import Iwhite from './Images/iw.png';
+import Ibrown from './Images/ib.png';
+import Wwhite from './Images/ww.png';
+import Wbrown from './Images/wb.png';
+import LogoWhite from './Images/lw.png';
+import LogoBrown from './Images/lb.png';
+import bg from './Images/bg.jpg';
+import Product from './Product.js';
 import './Styling.css';
+
+const useStyles = makeStyles(() => ({
+  buttonHeader:{
+    color:'white',
+    fontSize: '2.2vh',
+    margin:'0 0.6vw',
+    "&:hover": {
+      backgroundColor: 'transparent',
+      "&::after": {
+        width: '100%',
+        left: '0',
+      },
+    },
+    "&::after":{
+      background: 'none repeat scroll 0 0 white',
+      bottom: '0',
+      content: "''",
+      display: 'block',
+      height: '2px',
+      left: '50%',
+      position: 'absolute',
+      transition: 'width 0.3s ease 0s, left 0.3s ease 0s',
+      width:'0'
+    }
+  },
+  
+  buttonLogo:{
+    "&:hover": {
+      backgroundColor: 'transparent',
+    }
+  }
+}))
 
 const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
 // General scroll to element function
 
 const App = (props) => {
-    const produkRef = useRef(null)
-    const contactRef = useRef(null)
-    const marketPlaceRef = useRef(null)
-    const toProduk = () => scrollToRef(produkRef)
-    const toContact = () => scrollToRef(contactRef)
-    const toMarketPlace = () => scrollToRef(marketPlaceRef)
-    return (
-        <div>
-            <CssBaseline/>
-            <AppBar>
-                <Toolbar style={{backgroundColor:'white'}}>
-                  {
-                    [
-                      {name:'Produk',toRef:toProduk},
-                      {name:'Contact',toRef:toContact},
-                      {name:'Marketplace',toRef:toMarketPlace}
-                    ].map((item)=>(
-                      <Grid container item xs={1} justify='center'>
-                        <Button onClick={item.toRef} className='buttonHeader' disableRipple>
-                          <div>
-                            {item.name}
-                          </div>
-                        </Button>
-                      </Grid>
-                    ))
-                  }
-                    <Grid container item xs={7} justify='center' style={{color:'black'}}>
-                      Logo
-                    </Grid>
-                    <Grid container item xs={3} justify='flex-end'>
-                      {
-                        [
-                          {img:'/images/facebook.png'},
-                          {img:'/images/whatsapp.png'},
-                          {img:'/images/instagram.png'},
-                        ].map((item,index)=>(
-                          <img src={item.img} alt={index} className='iconSize'/>
-                        ))
-                      }
-                    </Grid>
-                </Toolbar>
-            </AppBar>
+  const homeRef = useRef(null);
+  const ProductRef = useRef(null);
+  const contactRef = useRef(null);
+  const marketPlaceRef = useRef(null);
+  const toHome = () => scrollToRef(homeRef);
+  const toProduct = () => scrollToRef(ProductRef);
+  const toContact = () => scrollToRef(contactRef);
+  const toMarketPlace = () => scrollToRef(marketPlaceRef);
+  const classes = useStyles();
+  return (
+      <div>
+          <CssBaseline/>
+          <AppBar>
+              <Toolbar style={{backgroundColor:'#7a4a16'}}>
+                {
+                  [
+                    {name:'Product',toRef:toProduct},
+                    {name:'Marketplace',toRef:toMarketPlace},
+                    {name:'Contact',toRef:toContact},
+                  ].map((item)=>(
+                      <Button onClick={item.toRef} className={classes.buttonHeader} disableRipple>
+                        <div>
+                          {item.name}
+                        </div>
+                      </Button>
+                  ))
+                }
+                  <Grid container item xs={7} justify='center' style={{color:'black'}} ref={homeRef}>
+                    <Button onClick={toHome} disableRipple className={classes.buttonLogo}>
+                      <img src={LogoWhite} alt='logo' className='logoHeader'/>
+                    </Button>
+                  </Grid>
 
-            <div>
-              <Grid className='background' container justify='center' alignItems='center'>
-              </Grid>
-              
+                  <Grid container item xs={3} justify='flex-end'>
+                    {
+                      [
+                        {img:Iwhite},
+                        {img:FBwhite},
+                        {img:Wwhite},
+                      ].map((item,index)=>(
+                        <img src={item.img} alt={index} className='iconHeader'/>
+                      ))
+                    }
+                  </Grid>
+              </Toolbar>
+          </AppBar>
+
+          <div>
+            {/* Front Page */}
+            <Grid className='background' container justify='center' alignItems='center'>
+              <img src={bg} alt='randompic' className='frontPageImage'/>
+            </Grid>
+            
+            <div className='background' ref={ProductRef}>
+              {/* Product Title */}
               <Grid 
-                className='produk' 
-                ref={produkRef} 
+                className='product' 
                 container justify='center' alignItems='flex-end' 
               >
-                  Produk
+                  Product
               </Grid>
-
+              
+              {/* Product Content */}
               <Grid container>
                 {
-                  Produk.map((item)=>(
-                    <Grid className='isiProduk' item md={4} xs={6} container justify='center' alignItems='center'>
+                  Product.map((item)=>(
+                    <Grid className='isiProduct' item xs={12} container alignItems='center'>
                       <img src={item.img} alt={item.name} className='imgSize'/>
                       <div>
-                        <div className='namaProduk'>
+                        <div className='namaProduct'>
                           {item.name}
+                          <div className='descProduct'>
+                            {item.desc}
+                          </div>
                         </div>
                         {
                           item.price.map((list)=>(
-                            <div className='hargaProduk250'>
+                            <div className='hargaProduct'>
                               {list.weight}
                             </div>
                           ))
@@ -83,37 +136,50 @@ const App = (props) => {
                   ))
                 }
               </Grid>
-
-              <Grid className='contact' ref={contactRef} container justify='center'>
-                <div>
-                  Stay in Touch!
-                </div>
-
-                <Grid>
-
-                </Grid>
-              </Grid>
-
-              <Grid className='marketPlace' ref={marketPlaceRef}>
-
-              </Grid>
-
-              <Grid container className='footer'>
-                <Grid item xs={5} container justify='center' alignItems='center' >
-                  Logo
-                </Grid>
-                <Grid item xs={4} style={{display:'inline-block',textAlign:'center'}} >
-                  <div style={{padding:'5vh 0 0'}}>
-                    Contact us!
-                  </div>
-                  <div style={{fontSize:'2vh'}}>
-                    Phone Number
-                  </div>
-                </Grid>
-              </Grid>
             </div>
-        </div>
-    );
+            
+            {/* MarketPlace */}
+            <Grid className='marketPlace' ref={marketPlaceRef}>
+                <Grid item xs={12} className='marketPlaceHead' container justify='center'>
+                  Marketplace
+                </Grid>
+                <Paper className='marketPlacePaper'>
+
+                <Grid item md={3} xs={6} container justify='center'>
+
+                </Grid>
+                </Paper>
+            </Grid>
+
+            {/* Footer and Contact */}
+            <Grid container className='footer' ref={contactRef}>
+              <Grid item xs={6} container justify='center' alignItems='center' >
+                <img src={LogoBrown} alt='logoFooter' className='logoFooter'/>
+              </Grid>
+              <Grid item xs={6}  container justify='center' alignItems='center'>
+                <div className='footerText'>
+                  Hubungi kami
+
+                  <div>
+                    abadicoffee788@gmail.com
+                  </div>
+
+                  {
+                    [
+                      {img:Ibrown},
+                      {img:FBbrown},
+                      {img:Wbrown},
+                    ].map((item,index)=>(
+                      <img src={item.img} alt={index} className='iconFooter'/>
+                    ))
+                  }
+                </div>
+                
+              </Grid>
+            </Grid>
+          </div>
+      </div>
+  );
 }
 
 export default App
--- src/Product.js
+++ src/Product.js
@@ -1,52 +1,24 @@
- const Produk = [
-    { name:'Bubuk Arabica',
-      img:'/images/bubuk.png',
-      price:[
-        {weight:'250gr - Rp. 50.000,00'},
-        {weight:'500gr - Rp. 100.000,00'},
-        {weight:'1kg - Rp. 200.000,00'}
-      ]
-    },
-    { name:'Bubuk Arabica',
-      img:'/images/bubuk.png',
-      price:[
-        {weight:'250gr - Rp. 50.000,00'},
-        {weight:'500gr - Rp. 100.000,00'},
-        {weight:'1kg - Rp. 200.000,00'}
-      ]
-    },
-    { name:'Bubuk Arabica',
-      img:'/images/bubuk.png',
-      price:[
-        {weight:'250gr - Rp. 50.000,00'},
-        {weight:'500gr - Rp. 100.000,00'},
-        {weight:'1kg - Rp. 200.000,00'}
-      ]
-    },
-    { name:'Bubuk Arabica',
-      img:'/images/bubuk.png',
-      price:[
-        {weight:'250gr - Rp. 50.000,00'},
-        {weight:'500gr - Rp. 100.000,00'},
-        {weight:'1kg - Rp. 200.000,00'}
-      ]
-    },
-    { name:'Bubuk Arabica',
-      img:'/images/bubuk.png',
-      price:[
-        {weight:'250gr - Rp. 50.000,00'},
-        {weight:'500gr - Rp. 100.000,00'},
-        {weight:'1kg - Rp. 200.000,00'}
-      ]
-    },
-    { name:'Bubuk Arabica',
-      img:'/images/bubuk.png',
-      price:[
-        {weight:'250gr - Rp. 50.000,00'},
-        {weight:'500gr - Rp. 100.000,00'},
-        {weight:'1kg - Rp. 200.000,00'}
-      ]
-    },
-  ]
+import bubuk from './Images/bubuk.png'
+
+const Produk = [
+  { name:'Bubuk Arabica',
+    img:bubuk,
+    desc:'Bubuk kopi halus dari arabica, terbuat dari biji pilihan terbaik',
+    price:[
+      {weight:'250gr - Rp. 50.000,00'},
+      {weight:'500gr - Rp. 100.000,00'},
+      {weight:'1kg - Rp. 200.000,00'}
+    ]
+  },
+  { name:'Bubuk Arabica',
+    img:bubuk,
+    desc:'Bubuk kopi halus dari arabica, terbuat dari biji pilihan terbaik',
+    price:[
+      {weight:'250gr - Rp. 50.000,00'},
+      {weight:'500gr - Rp. 100.000,00'},
+      {weight:'1kg - Rp. 200.000,00'}
+    ]
+  },
+]
 
 export default Produk
